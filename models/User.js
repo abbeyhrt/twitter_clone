69032ff1833268a39381b851644ce29c277c0163
Abbey Hart <abbeyhrt@gmail.com>
2017-10-19 15:56:36 -0500

    Add user abilities, in part

--- models/User.js
+++ models/User.js
@@ -1,3 +1,24 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const md5 = require('md5');
+const validator = require('validator');
+
+const User = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, 'Inavlied Email Address'],
+    required: 'An email address is required',
+  },
+  name: {
+    type: String,
+    required: 'Please supply a name!',
+    trim: true,
+  },
+});
+
+User.plugin(passportLocalMongooseEmail);
 
 module.exports = mogoose.model('User');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/login', userController.loginForm);
 
 module.exports = router;
